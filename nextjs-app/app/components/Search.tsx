0f9689d2d138963f11260fadf8b9a48a7f2ca3fe
Chaim Baror <chaim.baror@mapcore.onmicrosoft.com>
2024-11-10 10:09:55 +0200

    fix next style and add page message

--- nextjs-app/app/components/Search.tsx
+++ nextjs-app/app/components/Search.tsx
@@ -13,9 +13,8 @@ export const Search: React.FC<SearchProps> = ({ placeholder }) => {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  
+
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
 
@@ -27,20 +26,32 @@ export const Search: React.FC<SearchProps> = ({ placeholder }) => {
     replace(`${pathname}?${params.toString()}`);
   }, 300);
 
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('query')?.toString()}
+  // Clear the search field and reset search params
+  const handleClear = () => {
+    const params = new URLSearchParams(searchParams);
+    params.delete('query');
+  };
 
-      />
-    </div>
+  return (
+    <form className="max-w-md mx-auto">
+      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only text-white">Search</label>
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg className="w-4 h-4 text-gray-500 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+          </svg>
+        </div>
+        <input
+          defaultValue={searchParams.get('query')?.toString()}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder={placeholder} required />
+        <button onClick={handleClear} type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">clear</button>
+      </div>
+    </form>
+
   );
 }
+
+
+
---
+++ nextjs-app/ui/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+export const Navbar = () => {
+    return (
+        <nav className=" bg-gray-800 border-gray-700">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Messages</span>
+                </a>
+                <button data-collapse-toggle="navbar-hamburger" type="button" className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+                <div className="hidden w-full" id="navbar-hamburger">
+                    <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-800 border-gray-700">
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-white rounded bg-blue-600" aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 text-gray-400 hover:bg-gray-700 hover:text-white">Services</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 text-gray-400 md:dark:hover:text-white hover:bg-gray-700 hover:text-white">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 text-gray-400 hover:bg-gray-700 hover:text-white">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
